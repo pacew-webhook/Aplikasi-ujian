0ef5426019e1768713621de99e0b489d9cfe7535
pacew-webhook <alamremix@gmail.com>
2026-08-30 00:09:21 +0900

    Add files via upload

--- app/src/main/java/com/example/aplikasiujian/ui/theme/Theme.kt
+++ app/src/main/java/com/example/aplikasiujian/ui/theme/Theme.kt
@@ -1,1 +1,17 @@
+package com.example.aplikasiujian.ui.theme
 
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+
+private val AppColorScheme = lightColorScheme()
+
+@Composable
+fun AplikasiUjianTheme(
+    content: @Composable () -> Unit
+) {
+    MaterialTheme(
+        colorScheme = AppColorScheme,
+        content = content
+    )
+}
