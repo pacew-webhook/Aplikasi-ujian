b4ac91a9af382ed4cf997ad9b670e0d27f5f8a91
pacew-webhook <alamremix@gmail.com>
2026-08-30 00:05:58 +0900

    Add files via upload

--- app/src/main/java/com/example/aplikasiujian/MainActivity.kt
+++ app/src/main/java/com/example/aplikasiujian/MainActivity.kt
@@ -1,1 +1,18 @@
+package com.example.aplikasiujian
 
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.example.aplikasiujian.ui.UjianApp
+import com.example.aplikasiujian.ui.theme.AplikasiUjianTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AplikasiUjianTheme {
+                UjianApp()
+            }
+        }
+    }
+}
